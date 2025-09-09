d8f71da0bafd52c814c871181b1ae3f30ff29a0d
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-09 15:05:02 -0300

    feat(entrega): adiciona atributos, atributos, metodos construtores com parametro e getters e setters

---
+++ src/main/java/org/example/model/Entrega.java
@@ -0,0 +1,77 @@
+package org.example.model;
+
+import org.example.enums.StatusEntrega;
+
+public class Entrega {
+    private int id;
+    private int pedido_id;
+    private int motorista_id;
+    private String data_saida;
+    private String data_entrega;
+    private StatusEntrega status;
+
+    public Entrega(int id, int pedido_id, int motorista_id, String data_saida, String data_entrega, StatusEntrega status) {
+        this.id = id;
+        this.pedido_id = pedido_id;
+        this.motorista_id = motorista_id;
+        this.data_saida = data_saida;
+        this.data_entrega = data_entrega;
+        this.status = status;
+    }
+
+    public Entrega(int pedido_id, int motorista_id, String data_saida, String data_entrega, StatusEntrega status) {
+        this.pedido_id = pedido_id;
+        this.motorista_id = motorista_id;
+        this.data_saida = data_saida;
+        this.data_entrega = data_entrega;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPedido_id() {
+        return pedido_id;
+    }
+
+    public void setPedido_id(int pedido_id) {
+        this.pedido_id = pedido_id;
+    }
+
+    public int getMotorista_id() {
+        return motorista_id;
+    }
+
+    public void setMotorista_id(int motorista_id) {
+        this.motorista_id = motorista_id;
+    }
+
+    public String getData_saida() {
+        return data_saida;
+    }
+
+    public void setData_saida(String data_saida) {
+        this.data_saida = data_saida;
+    }
+
+    public String getData_entrega() {
+        return data_entrega;
+    }
+
+    public void setData_entrega(String data_entrega) {
+        this.data_entrega = data_entrega;
+    }
+
+    public StatusEntrega getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEntrega status) {
+        this.status = status;
+    }
+}
