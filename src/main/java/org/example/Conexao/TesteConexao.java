c3192c5030b25bab9364b642fff8915a915ec905
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-09 15:08:31 -0300

    feat: adiciona classe TesteConexao para validar conexão com o banco

---
+++ src/main/java/org/example/Conexao/TesteConexao.java
@@ -0,0 +1,17 @@
+package org.example.Conexao;
+import java.sql.Connection;
+
+public class TesteConexao {
+
+    public static void main(String[] args) {
+            try (Connection conn = Conexao.conectar()) {
+                if (conn != null) {
+                    System.out.println("Conexão realizada com sucesso!");
+                } else {
+                    System.out.println("Falha na conexão.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+}
