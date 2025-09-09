8c962423339ec5da9303b4cf63818d6ba3725387
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-09 15:47:15 -0300

    feat: adiciona View geral

---
+++ src/main/java/org/example/view/ViewGeral.java
@@ -0,0 +1,16 @@
+package org.example.view;
+
+
+import java.util.Scanner;
+
+public class ViewGeral {
+    static Scanner scNum = new Scanner (System.in);
+    static Scanner scStr = new Scanner(System.in);
+
+    public static void menuGeral(){
+        System.out.println("""
+                ========= BEM VINDO AO KAFÉ ENTREGAS =========\n
+                
+                """);
+    }
+}
