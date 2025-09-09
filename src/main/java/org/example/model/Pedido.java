42c8deb4750045276bbfea182b7b8e8641959dac
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-09 15:05:32 -0300

    feat(Pedido): adiciona atributos, atributos, metodos construtores com parametro e getters e setters

---
+++ src/main/java/org/example/model/Pedido.java
@@ -0,0 +1,77 @@
+package org.example.model;
+
+import org.example.enums.StatusPedido;
+
+public class Pedido {
+    private int id;
+    private int cliente_id;
+    private String data_pedido;
+    private double volume_m3;
+    private double peso_kg;
+    private StatusPedido status;
+
+    public Pedido(int id, int cliente_id, String data_pedido, double volume_m3, double peso_kg, StatusPedido status) {
+        this.id = id;
+        this.cliente_id = cliente_id;
+        this.data_pedido = data_pedido;
+        this.volume_m3 = volume_m3;
+        this.peso_kg = peso_kg;
+        this.status = status;
+    }
+
+    public Pedido(int cliente_id, String data_pedido, double volume_m3, double peso_kg, StatusPedido status) {
+        this.cliente_id = cliente_id;
+        this.data_pedido = data_pedido;
+        this.volume_m3 = volume_m3;
+        this.peso_kg = peso_kg;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public String getData_pedido() {
+        return data_pedido;
+    }
+
+    public void setData_pedido(String data_pedido) {
+        this.data_pedido = data_pedido;
+    }
+
+    public double getVolume_m3() {
+        return volume_m3;
+    }
+
+    public void setVolume_m3(double volume_m3) {
+        this.volume_m3 = volume_m3;
+    }
+
+    public double getPeso_kg() {
+        return peso_kg;
+    }
+
+    public void setPeso_kg(double peso_kg) {
+        this.peso_kg = peso_kg;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+}
