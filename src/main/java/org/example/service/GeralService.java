b76314dc0dfc09bffbb3504f540293f1c9aded1d
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-11 20:30:15 -0300

    feat: adiciona GeralService como orquestrador de menus

---
+++ src/main/java/org/example/service/GeralService.java
@@ -0,0 +1,30 @@
+package org.example.service;
+
+
+import org.example.view.ViewCliente;
+import org.example.view.ViewGeral;
+
+import org.example.view.ViewGeral;
+import org.example.view.ViewMotorista;
+
+public class GeralService {
+    public void gerenciadorService(){
+
+        int opcao = ViewGeral.menuGeral();
+
+        switch (opcao){
+            case 1-> {
+                ClienteService clienteService = new ClienteService();
+                clienteService.gerenciadorCliente();
+            }
+            case 2->{
+                MotoristaService motoristaService = new MotoristaService();
+                motoristaService.gerenciadorMotorista();
+            }
+            case 3->{}
+            case 4->{}
+            case 0->{}
+
+        }
+    }
+}
