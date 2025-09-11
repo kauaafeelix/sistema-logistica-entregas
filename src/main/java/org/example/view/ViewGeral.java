0b09447e03b538ac66e2b8add309664a8eeaa9ea
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-11 20:33:09 -0300

    feat(view): adiciona ViewGeral com menu principal

--- src/main/java/org/example/view/ViewGeral.java
+++ src/main/java/org/example/view/ViewGeral.java
@@ -7,10 +7,17 @@ public class ViewGeral {
     static Scanner scNum = new Scanner (System.in);
     static Scanner scStr = new Scanner(System.in);
 
-    public static void menuGeral(){
+    public static int menuGeral(){
         System.out.println("""
-                ========= BEM VINDO AO KAFÉ ENTREGAS =========\n
-                
+                \n========= BEM VINDO A KAFE ENTREGAS =========\n
+                1- Gerenciar Clientes\n
+                2- Gerenciar Motoristas\n
+                3- Gerenciar Pedidos\n
+                4- Gerenciar Entregas\n
+                0 - Sair\n
+                \nDigite sua opção: 
                 """);
+        int opcao = scNum.nextInt();
+        return opcao;
     }
 }
