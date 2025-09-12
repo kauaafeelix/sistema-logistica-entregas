2fbf1aeb4dfd55b2ce0c2b0d6b168cfc4ed54937
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-11 21:21:22 -0300

    feat: adiciona metodo para buscar clientes pelo cpf ou cnpj

--- src/main/java/org/example/DAO/ClienteDAO.java
+++ src/main/java/org/example/DAO/ClienteDAO.java
@@ -1,11 +1,15 @@
 package org.example.DAO;
 
+import com.mysql.cj.xdevapi.Client;
 import org.example.model.Cliente;
 import org.example.Conexao.Conexao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteDAO {
     public void cadastrarCliente(Cliente cliente) throws SQLException{
@@ -26,4 +30,31 @@ public class ClienteDAO {
             stmt.executeUpdate();
         }
     }
+
+    public List<Cliente> buscarClientePorCpf(String cpf_cnpj) throws SQLException{
+        List<Cliente> clientes = new ArrayList<>();
+        String sql = "SELECT id, nome, cpf_cnpj, endereco, cidade, estado FROM Cliente WHERE cpf_cnpj = ?";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, cpf_cnpj);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String cpf_cnpj_banco = rs.getString("cpf_cnpj");
+                String endereco = rs.getString("endereco");
+                String cidade = rs.getString("cidade");
+                String estado = rs.getString("estado");
+
+                var cliente = new Cliente(id, nome, cpf_cnpj_banco, endereco, cidade, estado);
+                clientes.add(cliente);
+            }
+        }
+        return clientes;
+    }
+
 }
