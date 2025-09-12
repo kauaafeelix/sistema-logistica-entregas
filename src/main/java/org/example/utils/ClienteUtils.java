9ef28c53b5e91c179f80eab1805b2ff4019d9378
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-11 21:43:20 -0300

    feat(utils): adiciona ClienteUtils para exibição de listas de clientes

---
+++ src/main/java/org/example/utils/ClienteUtils.java
@@ -0,0 +1,23 @@
+package org.example.utils;
+
+import org.example.model.Cliente;
+
+import java.util.List;
+
+public class ClienteUtils {
+    public static void exibirClientes(List<Cliente> clientes) {
+        if (!clientes.isEmpty()) {
+
+            for (Cliente cliente : clientes) {
+                System.out.println("\nID: " + cliente.getId());
+                System.out.println("NOME: " + cliente.getNome());
+                System.out.println("CPF/CNPJ: " + cliente.getCpf_cnpj());
+                System.out.println("ENDEREÇO: " + cliente.getEndereco());
+                System.out.println("CIDADE: " + cliente.getCidade());
+                System.out.println("ESTADO: " + cliente.getEstado());
+            }
+        } else {
+            System.out.println("Nenhum contato cadastrado.");
+        }
+    }
+}
