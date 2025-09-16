9909785f9e9c1475f94c28b039428995047732e9
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-15 21:36:23 -0300

    feat: finaliza logica de negocio no gerenciador de motorista

--- src/main/java/org/example/service/MotoristaService.java
+++ src/main/java/org/example/service/MotoristaService.java
@@ -1,21 +1,29 @@
 package org.example.service;
 
 import org.example.view.ViewCliente;
+import org.example.view.ViewGeral;
 import org.example.view.ViewMotorista;
 
 public class MotoristaService {
-    ViewMotorista motorista = new ViewMotorista();
     public void gerenciadorMotorista(){
         int opcao = ViewMotorista.menuMotorista();
 
         switch (opcao){
             case 1->{
-                motorista.cadastrarMotorista();
+                ViewMotorista.cadastrarMotorista();
             }
-            case 2->{}
-            case 3->{}
-            case 4->{}
-            case 0->{}
+            case 2->{
+                ViewMotorista.listarMotoristas();
+            }
+            case 3->{
+                ViewMotorista.buscarMotorista();
+            }
+            case 4->{
+                ViewMotorista.excluirMotorista();
+            }
+            case 0->{
+                ViewGeral.menuGeral();
+            }
         }
     }
 }
