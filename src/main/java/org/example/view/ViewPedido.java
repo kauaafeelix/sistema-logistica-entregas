bbe9160fe669efb01b02d89b898aa738e52c978f
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-17 18:35:11 -0300

    feat: adiciona metodo static para menu de Pedidos

--- src/main/java/org/example/view/ViewPedido.java
+++ src/main/java/org/example/view/ViewPedido.java
@@ -1,4 +1,26 @@
 package org.example.view;
 
+import org.example.model.Pedido;
+
+import java.util.Scanner;
+
 public class ViewPedido {
+
+    static Scanner scNum = new Scanner (System.in);
+    static Scanner scStr = new Scanner (System.in);
+
+    public static int menuPedido(){
+        System.out.println("========= GERENCIADOR DE PEDIDO =========\n");
+        System.out.println("""
+                1- Criar Pedido
+                2- Atribuir Pedido a um Motorista
+                3- Buscar Pedido por CPF/CNPJ do Cliente
+                4- Cancelar Pedido
+                5- Voltar ao Menu Principal
+                """);
+        int opcao = scNum.nextInt();
+        return opcao;
+
+    }
+
 }
