b61142cae6ac539a125f17f057b7b3e78803be4a
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-17 21:03:37 -0300

    feat: adiciona metodo de criar pedido na logica de negocio no gerenciador de pedido

--- src/main/java/org/example/service/PedidoService.java
+++ src/main/java/org/example/service/PedidoService.java
@@ -1,4 +1,22 @@
 package org.example.service;
 
+import org.example.view.ViewGeral;
+import org.example.view.ViewPedido;
+
 public class PedidoService {
+    public void gerenciadorPedido(){
+        int opcao = ViewPedido.menuPedido();
+
+        switch (opcao){
+            case 1->{
+                ViewPedido.criarPedido();
+            }
+            case 2->{}
+            case 3->{}
+            case 4->{}
+            case 0->{}
+
+
+        }
+    }
 }
