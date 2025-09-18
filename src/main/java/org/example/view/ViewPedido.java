6920e1e2c2310cf2d99d6b6ec1d16f12c46856f2
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-17 21:05:10 -0300

    feat: cria metodo de criarPedido com interações

--- src/main/java/org/example/view/ViewPedido.java
+++ src/main/java/org/example/view/ViewPedido.java
@@ -1,7 +1,10 @@
 package org.example.view;
 
 import org.example.model.Pedido;
+import org.example.model.enums.StatusPedido;
+import org.example.utils.PedidoUtils;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class ViewPedido {
@@ -20,6 +23,29 @@ public class ViewPedido {
                 """);
         int opcao = scNum.nextInt();
         return opcao;
+
+    }
+
+    public static void criarPedido(){
+        System.out.println("========= CRIAR PEDIDO =========\n");
+
+        int id_cliente = PedidoUtils.existeIdCliente();
+        if (id_cliente != -1){
+
+        LocalDate date = PedidoUtils.data();
+
+        System.out.println("Digite o Volume do Pedido (em m³): ");
+        double volume_m3 = scNum.nextDouble();
+
+        System.out.println("Digite o Peso do Pedido (em kg): ");
+        double peso_kg = scNum.nextInt();
+
+        System.out.println("Digite o Status do Pedido (PENDENTE, ENTREGUE, CANCELADO): ");
+        StatusPedido status = StatusPedido.valueOf(scStr.nextLine());
+
+        }else{
+            System.out.println("ID do cliente não encontrado.");
+        }
 
     }
 
