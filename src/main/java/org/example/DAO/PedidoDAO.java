e15d7da58e6d1e0e8ad86264daadac08bdaa027c
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-18 00:37:52 -0300

    feat: cria query para criar pedido (com possivel mudança)

--- src/main/java/org/example/DAO/PedidoDAO.java
+++ src/main/java/org/example/DAO/PedidoDAO.java
@@ -1,4 +1,34 @@
 package org.example.DAO;
 
+import org.example.Conexao.Conexao;
+import org.example.model.Pedido;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PedidoDAO {
+    public void criarPedido(Pedido pedido) throws SQLException{
+        String sql = """
+                INSERT INTO Pedido (
+                cliente_id,
+                data_pedido,
+                volume_m3,
+                peso_kg,
+                status
+                VALUES (?,?,?,?,?)
+                """;
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1, pedido.getCliente_id());
+            stmt.setDate(2, Date.valueOf(pedido.getData_pedido()));
+            stmt.setDouble(3, pedido.getVolume_m3());
+            stmt.setDouble(4, pedido.getPeso_kg());
+            stmt.setString(5, String.valueOf(pedido.getStatus()));
+            stmt.executeUpdate();
+
+        }
+    }
 }
