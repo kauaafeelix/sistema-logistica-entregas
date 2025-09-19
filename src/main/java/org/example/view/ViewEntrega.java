42d28c1c1203d60ed7252ecfca69ab8f2a62b9c0
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-18 21:36:19 -0300

    feat: cria menu para gerenciar entregas

--- src/main/java/org/example/view/ViewEntrega.java
+++ src/main/java/org/example/view/ViewEntrega.java
@@ -1,4 +1,28 @@
 package org.example.view;
 
+import java.util.Scanner;
+
 public class ViewEntrega {
+    static Scanner scNum = new Scanner(System.in);
+    static Scanner scstr = new Scanner(System.in);
+
+    public static int menuEntrega(){
+        System.out.println("""
+                ========= GERENCIADOR DE ENTREGAS =========\n
+                1 - GERAR ENTREGA (ATRIBUIR A UM MOTORISTA)
+                2 - LISTAR TODAS ENTREGAS DE CLIENTE E MOTORISTA
+                3 - LISTAR TODAS AS ENTREGAS
+                4 - BUSCAR ENTREGA POR ID
+                5 - REGISTRAR EVENTO DE ENTREGA
+                6 - ATUALIZAR STATUS DE ENTREGA PARA ENTREGUE
+                7 - EXCLUIR ENTREGA
+                0 - VOLTAR AO MENU PRINCIPAL
+                ===========================================
+                
+                Digite sua opção: 
+                """);
+        int opcao = scNum.nextInt();
+
+        return opcao;
+    }
 }
