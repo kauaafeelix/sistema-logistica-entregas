3b85e6fb05448fcf4415c7b99346c0d69bbbb5d2
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-18 21:34:55 -0300

    feat: cria logica de service com switch case

--- src/main/java/org/example/service/EntregaService.java
+++ src/main/java/org/example/service/EntregaService.java
@@ -1,4 +1,19 @@
 package org.example.service;
 
+import org.example.view.ViewEntrega;
+
 public class EntregaService {
+    public void gerenciadorEntrega(){
+        int opcao = ViewEntrega.menuEntrega();
+
+        switch (opcao){
+            case 1->{}
+            case 2->{}
+            case 3->{}
+            case 4->{}
+            case 5->{}
+            case 6->{}
+            case 0->{}
+        }
+    }
 }
