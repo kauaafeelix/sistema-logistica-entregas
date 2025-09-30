35f00e1e27a2a6c454544da9c6a1e785ec4a6196
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-30 12:21:11 -0300

    feat: adiciona métodos para verificar a existência de ID de cliente e exibir entregas

--- src/main/java/org/example/utils/EntregaUtils.java
+++ src/main/java/org/example/utils/EntregaUtils.java
@@ -1,7 +1,10 @@
 package org.example.utils;
 
+import org.example.DAO.ClienteDAO;
 import org.example.DAO.MotoristaDAO;
 import org.example.DAO.PedidoDAO;
+import org.example.model.Cliente;
+import org.example.model.Entrega;
 import org.example.model.Motorista;
 import org.example.model.Pedido;
 
@@ -15,6 +18,8 @@ public class EntregaUtils {
     static Scanner scStr = new Scanner(System.in);
     static PedidoDAO pedidoDAO = new PedidoDAO();
     static MotoristaDAO motoristaDAO = new MotoristaDAO();
+    static ClienteDAO clienteDAO = new ClienteDAO();
+
 
     public static int existeIdPedido() {
         try {
@@ -50,5 +55,38 @@ public class EntregaUtils {
             e.printStackTrace();
         }
         return -1;
+    }
+
+    public static int existeIdCliente() {
+        try {
+            List<Cliente> clientes = clienteDAO.listarClientes();
+
+            System.out.println("Digite o ID do Cliente");
+            int id_cliente = scNum.nextInt();
+
+            for (Cliente c : clientes) {
+                if (c.getId() == id_cliente) {
+                    return id_cliente;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public static void exibirEntregas(List<Entrega> entregas) {
+        if (entregas.isEmpty() || entregas == null) {
+            System.out.println("Nenhuma Entrega cadastrada. ");
+        } else {
+            for (Entrega e : entregas) {
+                System.out.println("\nID DA ENTREGA: " + e.getId());
+                System.out.println("ID DO PEDIDO: " + e.getPedido_id());
+                System.out.println("NOME DO MOTORISTA:" +e.getMotorista_id());
+                System.out.println("DATA_SAIDA (YYYY:MM:DD): " + e.getData_saida());
+                System.out.println("DATA DA ENTREGA (YYYY:MM:DD): " + e.getData_entrega());
+                System.out.println("STATUS: " + e.getStatus());
+            }
+        }
     }
 }
