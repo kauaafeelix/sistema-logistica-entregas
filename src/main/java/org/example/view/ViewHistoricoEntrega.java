6d2b859bdaa3c7357ad815f7092724202640a729
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-30 10:48:24 -0300

    feat: adiciona menu de relatórios para visualização de entregas

--- src/main/java/org/example/view/ViewHistoricoEntrega.java
+++ src/main/java/org/example/view/ViewHistoricoEntrega.java
@@ -1,4 +1,23 @@
 package org.example.view;
 
+import java.util.Scanner;
+
 public class ViewHistoricoEntrega {
+    static Scanner scNum = new Scanner(System.in);
+    static Scanner scStr = new Scanner(System.in);
+
+    public static int RelatorioMenu(){
+
+        System.out.print("""
+                \n|| ==================== MENU RELATÓRIOS ==================== ||
+                || 1 - TOTAL DE ENTREGAS POR MOTORISTA
+                || 2 - CLIENTES COM MAIOR VOLUME ENTREGUE
+                || 3 - PEDIDOS PENDENTES POR ESTADO
+                || 4 - ENTREGAS ATRASADAS POR CIDADE
+                || 0 - VOLTAR AO MENU PRINCIPAL
+                || =====================================================
+                """);
+        int opcao = scNum.nextInt();
+        return opcao;
+}
 }
