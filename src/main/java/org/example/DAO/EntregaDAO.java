10354f13803d808770eb9ec37b042fc2efb8060a
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-30 11:18:42 -0300

    feat: implementa método para gerar entrega com dados do pedido e motorista

--- src/main/java/org/example/DAO/EntregaDAO.java
+++ src/main/java/org/example/DAO/EntregaDAO.java
@@ -1,4 +1,31 @@
 package org.example.DAO;
 
+import org.example.Conexao.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class EntregaDAO {
+
+    public void gerarEntrega(int idPedido, int idMotorista) throws SQLException {
+
+        String sql = """
+                INSERT INTO Entrega (
+                pedido_id,
+                motorista_id,
+                data_entrega,
+                status_entrega)
+                VALUES (?, ?, CURRENT_DATE, 'PENDENTE')
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1, idPedido);
+            stmt.setInt(2, idMotorista);
+            stmt.executeUpdate();
+
+        }
+    }
 }
