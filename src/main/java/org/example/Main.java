0e377a876ea22f747a5ca021a678ef9c80206d5f
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-01 15:14:20 -0300

    feat: altera o método main para permitir execução contínua do gerenciadorService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,16 @@ package org.example;
 
 import org.example.service.GeralService;
 
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Java
 public class Main {
     public static void main(String[] args) {
-
         GeralService gerenciador = new GeralService();
-
-        gerenciador.gerenciadorService();
+        int opcao;
+        do {
+            opcao = gerenciador.gerenciadorService();
+        } while (opcao != 0);
     }
 }
