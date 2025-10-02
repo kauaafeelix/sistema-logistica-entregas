43062093f6159b838361a33319b0381f58e2046a
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-02 18:29:21 -0300

    feat: adiciona método para listar clientes com maior volume entregue

--- src/main/java/org/example/DAO/HistoricoEntregaDAO.java
+++ src/main/java/org/example/DAO/HistoricoEntregaDAO.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class HistoricoEntregaDAO {
 
-    public void totalEntregasPorMotorista() throws SQLException{
+    public void totalEntregasPorMotorista() throws SQLException {
 
         String sql = """
                 SELECT m.nome AS motorista, COUNT(e.id) AS total_entregas
@@ -28,6 +28,34 @@ public class HistoricoEntregaDAO {
                 String motorista = rs.getString("motorista");
                 int totalEntregas = rs.getInt("total_entregas");
                 System.out.printf("%-20s | %-15d%n", motorista, totalEntregas);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clientesComMaiorVolumeEntregue() throws SQLException {
+        String sql = """
+                SELECT c.nome AS cliente, SUM(p.volume) AS total_volume
+                FROM Cliente c
+                JOIN Pedido p ON c.id = p.cliente_id
+                JOIN Entrega e ON p.id = e.pedido_id
+                GROUP BY c.nome
+                ORDER BY total_volume DESC;
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             var stmt = conn.prepareStatement(sql);
+             var rs = stmt.executeQuery()) {
+
+            System.out.printf("%-20s | %-15s%n", "Cliente", "Total Volume");
+            System.out.println("---------------------+----------------");
+
+            while (rs.next()) {
+                String cliente = rs.getString("cliente");
+                double totalVolume = rs.getDouble("total_volume");
+                System.out.printf("%-20s | %-15.2f%n", cliente, totalVolume);
             }
 
         } catch (SQLException e) {
