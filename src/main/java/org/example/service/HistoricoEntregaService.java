ce11e8b98a8325919862e7d12f88b5261638328b
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-02 18:25:24 -0300

    feat: adiciona gerenciador de histórico de entrega com opções de menu

--- src/main/java/org/example/service/HistoricoEntregaService.java
+++ src/main/java/org/example/service/HistoricoEntregaService.java
@@ -1,4 +1,25 @@
 package org.example.service;
 
+import org.example.view.ViewHistoricoEntrega;
+
 public class HistoricoEntregaService {
+
+    int opcao = ViewHistoricoEntrega.RelatorioMenu();
+
+    public void gerenciadorHistoricoEntrega(){
+        switch (opcao){
+            case 1->{
+                ViewHistoricoEntrega.totalEntregasPorMotorista();
+            }
+            case 2->{
+            }
+            case 3->{
+            }
+            case 4->{
+            }
+            case 0->{
+            }
+        }
+    }
+
 }
